e62861fb665604e43e9bb21ee2679440899b8cf0
ian mcgaunn <ian@mcgaunn.com>
2026-02-09 16:46:12 -0500

    basic script to invoke coreos-installer via podman

---
+++ scripts/proxmox_installer.sh
@@ -0,0 +1,6 @@
+STREAM="stable"
+# as an installed binary:
+#coreos-installer download -s $STREAM -p proxmoxve -f qcow2.xz --decompress -C /var/coreos
+# or as a container:
+podman run --pull=always --rm -v "./images:/data" -w /data \
+  quay.io/coreos/coreos-installer:release download -s $STREAM -p proxmoxve -f qcow2.xz --decompress
