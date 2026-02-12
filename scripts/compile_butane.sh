a00c53a7f55893e0341211e753aec077123be625
ian mcgaunn <ian@mcgaunn.com>
2026-02-11 19:53:16 -0500

    add a few more working butane configs

--- scripts/compile_butane.sh
+++ scripts/compile_butane.sh
@@ -5,8 +5,9 @@ set -o nounset
 
 SCRIPT_DIR="${0:A:h}"
 BUTANE_CONFIG="$1"
-CONFIGS_DIR="${SCRIPT_DIR}/../configs"
+CONFIGS_DIR="$(realpath ${SCRIPT_DIR}/../configs)"
 BUTANE_CONFIGS_DIR="${CONFIGS_DIR}/butane"
+BUTANE_FILES_DIR="${BUTANE_CONFIGS_DIR}/files"
 IGNITION_CONFIGS_DIR="${CONFIGS_DIR}/ignition"
 KEYS_DIR="$HOME/.ssh"
 
@@ -17,10 +18,11 @@ fi
 # compile input butane config to `ignition` directory with
 # `.bu` extension replaced with `.ign`
 IGNITION_CONFIG="${BUTANE_CONFIG:r}.ign"
-IGNITION_CONFIG_PATH="${IGNITION_CONFIGS_DIR}/${IGNITION_CONFIG}"
+IGNITION_CONFIG_PATH="$(realpath ${IGNITION_CONFIGS_DIR}/${IGNITION_CONFIG})"
 podman run --interactive --rm --security-opt label=disable \
   --volume ${BUTANE_CONFIGS_DIR}:/butane \
-  --volume ${KEYS_DIR}:/keys \
+  --volume ${BUTANE_FILES_DIR}:/files \
   --workdir /butane quay.io/coreos/butane:release \
-  -d /keys \
+  -d /files \
   --pretty --strict ${BUTANE_CONFIG} >${IGNITION_CONFIG_PATH}
+echo "compiled butane config to: ${IGNITION_CONFIG_PATH}"
